0db119f0d8bc351a4bee6e6d758f0fa7bbf0494a
trevorgraham <grahamtrevor@outlook.com>
2019-01-20 20:12:24 +0000

    An intial commit for two projects within music-guide, the main program and the music-guide-api

---
+++ music-guide/src/main/java/com/teegee/musicguide/MusicGuideApplication.java
@@ -0,0 +1,14 @@
+package com.teegee.musicguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicGuideApplication.class, args);
+	}
+
+}
+
